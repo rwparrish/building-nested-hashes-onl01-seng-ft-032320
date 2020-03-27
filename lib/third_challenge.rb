9b1cf3dc3d2ca332c3fc7ace45ac350892ffa058
Ryan Parrish <rino3717@gmail.com>
2020-03-27 02:15:57 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -4,7 +4,7 @@ epic_tragedy = {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => []
+      :hero_friends => [{}]
    },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
