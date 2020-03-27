c0619d1adcd5aad69bc94f4ecdc3cf26532c4f58
Ryan Parrish <rino3717@gmail.com>
2020-03-27 01:57:32 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -7,10 +7,10 @@ def second_challenge
       :hero_friends => []
       }, 
     :capulet => {
-      :patriarch
+      :patriarch =>
       :matriarch
       :heroine
-      
+      :heroine_friends
       }}
 
   
